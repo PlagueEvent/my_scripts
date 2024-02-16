92f9a13b0005c1e4d6b252d342044c58a4162c8f
Laptevepav <laptevep@it.mos.ru>
2024-02-16 16:25:02 +0300

    Навел красоту в скрипте kkd-vertica.py

--- kkd/kkd-vertica.py
+++ kkd/kkd-vertica.py
@@ -10,13 +10,17 @@ date_payload = json.dumps({
   "to": "2024-02-15"
 })
 
+#Для проверке на стейдже
+# date_payload = json.dumps({
+#   "snils": "202-619-243 25",
+# })
+
 '''
 Получаем админский токен
 '''
 headers = {
     'X-APPLICATION-TOKEN': 'oRZE4kiNJ3zdBRk9IN9EYmhO6yZ45EvfxQN2xCKQSkNql629JzERN4YT5HLux3vX',
 }
-
 json_data = {
     'email': 'admin@ex.com',
     'password': 'Xnp5kRZh',
@@ -26,25 +30,17 @@ response = requests.post('https://kkd-admin.data.corp/api/admin/auth/login',
                          headers=headers, json=json_data, verify=False)
 
 r = json.loads(response.text)
-
 admin_token = r.get('token')
 
 '''
 Отправляем запрос на целевую ручку
 '''
 url = "https://kkd-admin.data.corp/api/kkd-vertica-service/resend"
-#Для проверке на стейдже
-# date_payload = json.dumps({
-#   "snils": "202-619-243 25",
-# })
-
-
 headers = {
   'X-APPLICATION-TOKEN': 'oRZE4kiNJ3zdBRk9IN9EYmhO6yZ45EvfxQN2xCKQSkNql629JzERN4YT5HLux3vX',
   'X-AUTH-ADMIN': admin_token,
   'Content-Type': 'application/json'
 }
-
 response = requests.request("GET", url, headers=headers, data=date_payload,  verify=False)
 
 
